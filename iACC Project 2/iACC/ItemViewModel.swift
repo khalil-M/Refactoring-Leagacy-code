9e5865364d659c8a6d246620f48b999240885ec2
khalil-M <khail.mhelheli@gmail.com>
2022-02-02 22:45:47 +0100

    Replace untyped cell model with ItemViewModel

--- iACC Project 2/iACC/ItemViewModel.swift
+++ iACC Project 2/iACC/ItemViewModel.swift
@@ -10,29 +10,11 @@ struct ItemViewModel {
     
     init(_ item: Any, longDateStyle: Bool) {
         if let friend = item as? Friend {
-            title = friend.name
-            subtitle = friend.phone
+            self.init(friend: friend)
         } else if let card = item as? Card {
-            title = card.number
-            subtitle = card.holder
+            self.init(card: card)
         } else if let transfer = item as? Transfer {
-            let numberFormatter = Formatters.number
-            numberFormatter.numberStyle = .currency
-            numberFormatter.currencyCode = transfer.currencyCode
-            
-            let amount = numberFormatter.string(from: transfer.amount as NSNumber)!
-            title = "\(amount) • \(transfer.description)"
-            
-            let dateFormatter = Formatters.date
-            if longDateStyle {
-                dateFormatter.dateStyle = .long
-                dateFormatter.timeStyle = .short
-                subtitle = "Sent to: \(transfer.recipient) on \(dateFormatter.string(from: transfer.date))"
-            } else {
-                dateFormatter.dateStyle = .short
-                dateFormatter.timeStyle = .short
-                subtitle = "Received from: \(transfer.sender) on \(dateFormatter.string(from: transfer.date))"
-            }
+            self.init(transfer: transfer, longDateStyle: longDateStyle)
         } else {
             fatalError("unknown item: \(item)")
         }
